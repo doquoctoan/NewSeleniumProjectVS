70340c184e8b4bd77479b3c41ba6ccebb8f380d4
Ege Aksöz <eaksoz@kariyer.net>
2017-03-28 10:59:12 +0300

    Initial commit

---
+++ NewSeleniumProject/MyFirstTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewSeleniumProject
+{
+    public class MyFirstTest
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void myFirstTest()
+        {
+            driver.Navigate().GoToUrl("http://www.swtestacademy.com");
+
+            Assert.AreEqual("SW Test Academy - Software Test Academy", driver.Title);
+
+            driver.Close();
+
+            driver.Quit();
+        }
+    }
+}
